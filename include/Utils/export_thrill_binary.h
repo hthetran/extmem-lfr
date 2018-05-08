c65b100123a265980e29ea194284bed8a64863c2
Michael Hamann <michael.hamann@kit.edu>
2018-05-08 12:48:51 +0200

    export_thrill_binary: split graph into parts for parallel reading

--- include/Utils/export_thrill_binary.h
+++ include/Utils/export_thrill_binary.h
@@ -13,41 +13,47 @@
  */
     //! Append a varint to the writer.
 template <typename stream>
-stream& PutVarint(stream& s, uint64_t v) {
+size_t PutVarint(stream& s, uint64_t v) {
 	if (v < 128) {
 		s << uint8_t(v);
+		return 1;
 	}
-	else if (v < 128 * 128) {
+	if (v < 128 * 128) {
 		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
 		s << uint8_t((v >> 07) & 0x7F);
+		return 2;
 	}
-	else if (v < 128 * 128 * 128) {
+	if (v < 128 * 128 * 128) {
 		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
 		s << uint8_t((v >> 14) & 0x7F);
+		return 3;
 	}
-	else if (v < 128 * 128 * 128 * 128) {
+	if (v < 128 * 128 * 128 * 128) {
 		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
 		s << uint8_t((v >> 21) & 0x7F);
+		return 4;
 	}
-	else if (v < 128llu * 128 * 128 * 128 * 128) {
+	if (v < 128llu * 128 * 128 * 128 * 128) {
 		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 21) & 0x7F) | 0x80);
 		s << uint8_t((v >> 28) & 0x7F);
+		return 5;
 	}
-	else if (v < 128llu * 128 * 128 * 128 * 128 * 128) {
+	if (v < 128llu * 128 * 128 * 128 * 128 * 128) {
 		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 21) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 28) & 0x7F) | 0x80);
 		s << uint8_t((v >> 35) & 0x7F);
+		return 6;
 	}
-	else if (v < 128llu * 128 * 128 * 128 * 128 * 128 * 128) {
+	if (v < 128llu * 128 * 128 * 128 * 128 * 128 * 128) {
 		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
@@ -55,8 +61,9 @@ stream& PutVarint(stream& s, uint64_t v) {
 		s << uint8_t(((v >> 28) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 35) & 0x7F) | 0x80);
 		s << uint8_t((v >> 42) & 0x7F);
+		return 7;
 	}
-	else if (v < 128llu * 128 * 128 * 128 * 128 * 128 * 128 * 128) {
+	if (v < 128llu * 128 * 128 * 128 * 128 * 128 * 128 * 128) {
 		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
@@ -65,8 +72,9 @@ stream& PutVarint(stream& s, uint64_t v) {
 		s << uint8_t(((v >> 35) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 42) & 0x7F) | 0x80);
 		s << uint8_t((v >> 49) & 0x7F);
+		return 8;
 	}
-	else if (v < 128llu * 128 * 128 * 128 * 128 * 128 * 128 * 128 * 128) {
+	if (v < 128llu * 128 * 128 * 128 * 128 * 128 * 128 * 128 * 128) {
 		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
@@ -76,36 +84,56 @@ stream& PutVarint(stream& s, uint64_t v) {
 		s << uint8_t(((v >> 42) & 0x7F) | 0x80);
 		s << uint8_t(((v >> 49) & 0x7F) | 0x80);
 		s << uint8_t((v >> 56) & 0x7F);
-	}
-	else {
-		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 21) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 28) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 35) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 42) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 49) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 56) & 0x7F) | 0x80);
-		s << uint8_t((v >> 63) & 0x7F);
+		return 9;
 	}
 
-	return s;
+	s << uint8_t(((v >> 00) & 0x7F) | 0x80);
+	s << uint8_t(((v >> 07) & 0x7F) | 0x80);
+	s << uint8_t(((v >> 14) & 0x7F) | 0x80);
+	s << uint8_t(((v >> 21) & 0x7F) | 0x80);
+	s << uint8_t(((v >> 28) & 0x7F) | 0x80);
+	s << uint8_t(((v >> 35) & 0x7F) | 0x80);
+	s << uint8_t(((v >> 42) & 0x7F) | 0x80);
+	s << uint8_t(((v >> 49) & 0x7F) | 0x80);
+	s << uint8_t(((v >> 56) & 0x7F) | 0x80);
+	s << uint8_t((v >> 63) & 0x7F);
+	return 10;
 };
 
-void export_as_thrill_binary(EdgeStream &edges, node_t num_nodes, const std::string& filename) {
+void export_as_thrill_binary(EdgeStream &edges, node_t num_nodes, const std::string& filename, stxxl::external_size_type max_bytes = (1ul<<30)) {
 	edges.rewind();
 
-	std::ofstream out_stream(filename, std::ios::trunc | std::ios::binary);
+	size_t file_number = 0;
+
+	auto next_filename = [&]() {
+	    std::stringstream ss;
+	    ss << filename << ".part-" << std::setw(5) << std::setfill('0') << file_number;
+	    ++file_number;
+	    return ss.str();
+	};
+
+	std::ofstream out_stream(next_filename(), std::ios::trunc | std::ios::binary);
 	//out_stream << num_nodes << " " << num_edges << " " << 0 << std::endl;
 	
 	std::vector<node_t> neighbors;
+	stxxl::external_size_type bytes_written = 0;
 	for (node_t u = 0; u < num_nodes; ++u) {
 		neighbors.clear();
 		for (; !edges.empty() && edges->first == u; ++edges) {
 			neighbors.push_back(edges->second);
 		}
-		PutVarint(out_stream, neighbors.size());
+
+		// assume the size of the neighbors needs 4 bytes
+		if (bytes_written > 0 && bytes_written + neighbors.size() * 4 + 4 > max_bytes) {
+		    out_stream.close();
+		    // This does not compile with GCC < 5 because of a missing move assignment operator!
+		    // see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=54316 for a related issue
+		    out_stream = std::ofstream(next_filename(), std::ios::trunc | std::ios::binary);
+		    bytes_written = 0;
+		}
+
+		bytes_written += 4llu * neighbors.size();
+		bytes_written += PutVarint(out_stream, neighbors.size());
 
 		for (node_t v : neighbors) {
 			static_assert(std::is_same<int32_t, node_t>::value, "Node type is not int32 anymore, adjust code!");
